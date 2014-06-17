16b9fc0cf757e943376a1d8571a5254d60c4de72
Ward Kennes <=>
2014-06-17 15:16:23 +0200

    Remove the layout folder

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-2014_silexblog
+Silexblog
 ==============
+
+Learning the way silex works and how to create a simple basic weblog.
+
+Step 1:
+Showing posts from the database.
+
+Step 2:
+Registration
+
+Step 3:
+Create a post by some reigstered user.
+
+Step 4:
+Manage Posts
 
 Learning Silex - Creating a basic blog.
--- src/controllers.php
+++ src/controllers.php
@@ -1,16 +1,18 @@
 <?php
-$app->match('/', function(\Symfony\Component\HttpFoundation\Request $request) use ($app){
+/**
+ * Setting up some basic routing.
+ *
+ */
+$app->get('/home', function() use($app) {
 
-    /* Load the main template file. */
-    return $app['twig']->render('layouts/layout.html.twig', array());
+   return $app['twig']->render('home.html.twig', array());
+});
 
-})->bind('home');
 
+/**
+ * No page found? Throw an error...
+ */
 $app->error(function (\Exception $e, $code) use ($app){
-   return 'Sorry the requested page could not be found...';
+    return 'Sorry the requested page could not be found...';
 });
 
-
-$app->get('home', function() use ($app){
-    return $app['twig']->render('home.html.twig', array());
-});
